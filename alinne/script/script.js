591ae1980c3d584280202e0ba2eb7c9205c598d1
asefrigg <111322170+aseFigg@users.noreply.github.com>
2022-08-15 21:57:54 -0400

    Add files via upload

---
+++ alinne/script/script.js
@@ -0,0 +1,18 @@
+$("#btOla").click(
+    function(){
+    //     cadastro = 
+    //     {
+    //         'Nome':document.getElementById('inputNome').value,
+    //         'Endereco':document.getElementById('inputEndereco').value,
+    //         'Email':document.getElementById('inputEmail').value
+    //     }
+    // alert('Meu Nome é: ' +cadastro.Nome + ' sou de ' + cadastro.Endereco);
+
+            calculo =
+                {
+                    'a':document.getElementById('inputA').value,
+                    'b':document.getElementById('inputB').value
+                }
+                    alert(parseInt(calculo.a) + parseInt(calculo.b));
+    }
+)
